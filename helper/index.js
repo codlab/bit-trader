890295ee70b7f422d29acd49092f5067357d11c0
Kevin Le Perf <codlabtech@gmail.com>
2017-07-27 19:23:54 +0200

    add methods to ceil/floor

--- helper/index.js
+++ helper/index.js
@@ -6,5 +6,19 @@ module.exports = {
         amount = parseFloat((amount * multiplicator).toFixed(11));
 
         return Number((Math.round(amount) / multiplicator).toFixed(digits));
+    },
+    ceil: function (amount, digits = 5) {
+        const multiplicator = Math.pow(10, digits);
+
+        amount = parseFloat((amount * multiplicator).toFixed(11));
+
+        return Number((Math.ceil(amount) / multiplicator).toFixed(digits));
+    },
+    floor: function (amount, digits = 5) {
+        const multiplicator = Math.pow(10, digits);
+
+        amount = parseFloat((amount * multiplicator).toFixed(11));
+
+        return Number((Math.floor(amount) / multiplicator).toFixed(digits));
     }
 };
