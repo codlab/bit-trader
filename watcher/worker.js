270a81056e704e11690285591a777046a993f2e1
Kevin Le Perf <codlabtech@gmail.com>
2017-07-30 01:40:24 +0200

    add few recent statistic calculations

--- watcher/worker.js
+++ watcher/worker.js
@@ -13,72 +13,129 @@ const sendLogMessage = ipcHelper.sendLogMessage;
 const abs = Math.abs;
 
 module.exports = class WatcherWorker extends ChildSlave(EventEmitter) {
-    constructor(options, params) {
-        super(...arguments);
+  constructor(options, params) {
+    super(...arguments);
 
-        this._api = new KrakenWrapper(options.key, options.secret);
-        this._requestInterval = params.interval;
-        this._pair = params.pair;
-        this._interval = null;
+    this._api = new KrakenWrapper(options.key, options.secret);
+    this._requestInterval = params.interval;
+    this._pair = params.pair;
+    this._interval = null;
+  }
+
+  start(params) {
+    if (params) {
+      let newInterval = params.interval;
+      let newPair = params.pair;
+
+      if (newInterval && newPair) {
+        this._requestInterval = newInterval;
+        this._pair = newPair;
+      } else {
+        sendLogMessage('error', 'cannot updated worker params, because of missing params');
+      }
+    } else {
+      sendLogMessage('info', 'starting to work');
+      this._interval = setInterval(this.requestData.bind(this), this._requestInterval);
     }
+  }
 
-    start(params) {
-        if (params) {
-            let newInterval = params.interval;
-            let newPair = params.pair;
+  stop(params) {
+    if (params) {
+      sendLogMessage('error', `this worker doesn't support params within the stop action`);
+    } else {
+      clearInterval(this._interval);
+    }
+  }
 
-            if (newInterval && newPair) {
-                this._requestInterval = newInterval;
-                this._pair = newPair;
-            } else {
-                sendLogMessage('error', 'cannot updated worker params, because of missing params');
-            }
-        } else {
-            sendLogMessage('info', 'starting to work');
-            this._interval = setInterval(this.requestData.bind(this), this._requestInterval);
+  requestData() {
+    this._api.getTickForPair(this._pair).then((data) => {
+      return this.prepareData(data);
+    }).then((data) => {
+      this._api.getOHLCForPair(this._pair).then((infos) => {
+
+        if(data) {
+          data.infos = this.prepareHistoricalData(infos);
+
+          const calculated = data.infos.calc_open;
+          if(!isNaN(calculated)) {
+            data.current = calculated;
+          }
         }
-    }
 
-    stop(params) {
-        if (params) {
-            sendLogMessage('error', `this worker doesn't support params within the stop action`);
-        } else {
-            clearInterval(this._interval);
+        this.emit('data', data);
+      }).catch(e => {
+        console.log(e);
+        this.emit('data', data);
+      })
+    });
+  }
+
+  prepareHistoricalData(data) {
+    const preparedHistoricalData = {
+      avg_open: 0,
+      avg_count: 0
+    };
+
+    if(data && data[this._pair]) {
+      const array = data[this._pair];
+
+      var test_count = 0;
+
+      const calc = (last_n) => {
+        var i = array.length - last_n;
+        if(i < 0) i = 0;
+        for(; i < array.length; i++) {
+          preparedHistoricalData.avg_open += Number(array[i][1]);
+          preparedHistoricalData.avg_count ++;
         }
+      }
+
+      calc(20);
+      //now do it for the last 5 - once more
+      calc(5);
+      //and the last 2
+      calc(2);
+
+      //now we have :
+      //from the end-20 := 1[-20] 1[-19] ... 1[-5] 2[-4] 2[-3] 2[-2] 3[-1] 3[0]
+      //avg is based on the cardinal : 20 + 5 + 2 instead of 20
+      //to make sure we give more "relevance" to the last values / tendancies
+      //quite easily
+
+
+      const calculated = preparedHistoricalData.avg_open / preparedHistoricalData.avg_count;
+      if(!isNaN(calculated)) {
+        preparedHistoricalData.calc_open = calculated;
+      }
     }
 
-    requestData() {
-        this._api.getTickForPair(this._pair).then((data) => {
-            return this.prepareData(data);
-        }).then((data) => {
-            this.emit('data', data);
-        });
-    }
+    return preparedHistoricalData;
+  }
 
-    prepareData(data) {
-        const pair = this._pair;
-        data = _.get(data, pair);
+  prepareData(data) {
+    const pair = this._pair;
+    data = _.get(data, pair);
 
-        const low = data['l'].map(Number);
-        const high = data['h'].map(Number);
+    const low = data['l'].map(Number);
+    const high = data['h'].map(Number);
 
-        const preparedData = {
-            pair: pair,
-            low: {
-                today: low[0],
-                "24h": low[1]
-            },
-            high: {
-                today: high[0],
-                "24h": high[1]
-            },
-            volatility: {
-                today: abs(((high[0] - low[0]) / low[0]) * 100),
-                "24h": abs(((high[1] - low[1]) / low[1]) * 100)
-            },
-            current: Number(data['c'][0])
-        };
+    const preparedData = {
+      pair: pair,
+      low: {
+        today: low[0],
+        "24h": low[1]
+      },
+      high: {
+        today: high[0],
+        "24h": high[1]
+      },
+      volatility: {
+        today: abs(((high[0] - low[0]) / low[0]) * 100),
+        "24h": abs(((high[1] - low[1]) / low[1]) * 100)
+      },
+      current: Number(data['c'][0])
+    };
 
-        return preparedData;
-    }
+    return preparedData;
+  }
 };
