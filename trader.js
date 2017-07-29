0bbc34ad882767d8b0e1378d311a46171ceb2d57
Kevin Le Perf <codlabtech@gmail.com>
2017-07-30 01:41:02 +0200

    update main script to reflect auto-gen

--- trader.js
+++ trader.js
@@ -1,66 +1,50 @@
 "use strict";
 
-// const _ = require('lodash');
+const WatcherNew = require("./watcher");
+const reporter = require("./reporter");
+const StrategySpawner = require("./strategies");
 
-// const join = require('path').join;
+const krakenConfig = require("./config/kraken.json");
+const traderConfig = require("./config/trader.json");
 
-// const Watcher = require('./watcher');
-const reporter = require('./reporter');
-const StrategySpawner = require('./strategies');
-// const KrakenWrapper = require('./kraken');
+const key = krakenConfig.key;
+const secret = krakenConfig.secret;
 
-const krakenConfig = require('./config/kraken.json');
-// const telegramConfig = require('./config/telegram.json');
-
-// const LOG_PREFIX = '<bit-trader:trader>';
-
-// const watcher = new Watcher(krakenConfig.key, krakenConfig.secret);
-// const consoleReporter = new reporter.console(watcher);
-// const jsonfileReporter = new reporter.jsonFile(watcher, join(__dirname, 'json-storage'));
-// const telegramReporter = new reporter.telegram(watcher, telegramConfig.key, telegramConfig.userId);
-//
-// _.forEach(krakenConfig.pairs, (pair) => {
-// 	watcher.createWatcherWorkerForPair(pair, 60000);
-// });
-//
-// watcher.startWatching();
-
-// const strateger = new StrategySpawner(krakenConfig.key, krakenConfig.secret);
-//
-// strateger.createTrader('EOSEUR', 'micro-trades', {
-// 	minDiff: 0.05,
-// 	multiplicator: 2,
-// 	maxMoneyToUse: 5
-// });
-//
-// watcher.on('data', (data) => {
-// 	strateger.sendDataToWorker('EOSEUR', 'micro-trades', {
-// 		current: data.data.current,
-// 		volatility: data.volatility['24h']
-// 	});
-// });
-
-const money = krakenConfig.pairs[0];
-const WatcherNew = require('./watcher');
-const watcher = new WatcherNew(krakenConfig.key, krakenConfig.secret, money.pair);
-const strateger = new StrategySpawner(krakenConfig.key, krakenConfig.secret);
-
-strateger.createTrader(money, 'micro-trades', {
-  microPercent: 0.2,
-  minDiff: 10,
-  minSellDiff: 100,
-  multiplicator: 3,
-  maxMoneyToUse: 100
-});
-
-watcher.on('data', (data) => {
-  strateger.sendDataToWorker(money, 'micro-trades', {
-    current: data.current,
-    volatility: data.volatility['24h']
+function getPairForName(name) {
+  const list = krakenConfig.pairs.filter(pair => {
+    return pair && pair.pair && pair.pair === name;
   });
+  if(list.length > 0) return list[0];
+  return undefined;
+}
+
+const watchers = [];
+
+traderConfig.strategies.forEach(strategy => {
+  const money = getPairForName(strategy.pair);
+  if(money) {
+    const watcher = new WatcherNew(key, secret, money.pair);
+    const strateger = new StrategySpawner(key, secret);
+
+    strateger.createTrader(money, strategy.algorithm, strategy.options);
+
+    watcher.on("data", (data) => {
+      strateger.sendDataToWorker(money, strategy.algorithm, {
+        current: data.current,
+        volatility: data.volatility["24h"]
+      });
+    });
+
+    watchers.push(watcher);
+  } else {
+    console.error("unknown pair");
+  }
 });
 
 
-const consoleReporter = new reporter.console(watcher);
+watchers.forEach(watcher => {
+  console.log("starting watcher");
+  const consoleReporter = new reporter.console(watcher);
 
-watcher.start();
+  watcher.start();
+});
